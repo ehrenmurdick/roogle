b5a1e424d33dd4ad5d0291998e0cd1424271aff5
Ehren Murdick <ehren.murdick@gmail.com>
2010-09-28 09:26:46 -0700

    multiple pages of results

--- lib/roogle.rb
+++ lib/roogle.rb
@@ -1,3 +1,7 @@
+require 'mechanize'
+require 'term/ansicolor'
+require 'launchy'
+
 class Roogle
   include Term::ANSIColor
 
@@ -8,15 +12,8 @@ class Roogle
     @mech = ::Mechanize.new { |agent| agent.user_agent_alias = 'Mac Safari' }
   end
 
-  def do_search(query)
-    search_result = nil
-    @mech.get(THE_GOOGLE) do |page|
-      search_result = page.form_with(:name => 'f') do |search|
-        search.q = ARGV.join(' ')
-      end.submit
-    end
-
-    search_result.parser.css("h3.r a").each do |link|
+  def scan_page(page)
+    page.parser.css("h3.r a").each do |link|
       @results << link
       print red, "%2s " % [@results.size], reset
       print yellow, link.text[0..60].ljust(62), reset
@@ -27,8 +24,27 @@ class Roogle
 
     print ":"
 
-    n = STDIN.gets.chomp.to_i
+    n = STDIN.gets.chomp
 
-    ::Launchy.open(@results[n - 1].attr('href')) unless n < 1
+    if n == 'n'
+      page = page.links.find {|l| l.text == "Next" }.click
+      scan_page(page)
+    elsif n == 'p'
+      page = page.links.find {|l| l.text == "Previous" }.click
+      scan_page(page)
+    end
+
+    ::Launchy.open(@results[n.to_i - 1].attr('href')) unless n.to_i < 1
+  end
+
+  def do_search(query)
+    search_result = nil
+    @mech.get(THE_GOOGLE) do |page|
+      search_result = page.form_with(:name => 'f') do |search|
+        search.q = query
+      end.submit
+    end
+
+    scan_page(search_result)
   end
 end
